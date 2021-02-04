83807fa3c61b0123f24808769c97f15cb06cb876
Vasiliy Merzlykov <v.merov@yandex.ru>
2021-02-04 15:20:57 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { ShopItemFuncClassBased } from './components/ShopItemFunc/ShopItemFuncClassBased';
+import { Item } from './model/Item';
 
 function App() {
+  const item = new Item();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="background-element">
+      </div>
+      <div className="highlight-window">
+        <div className='highlight-overlay'></div>
+      </div>
+      <div className="window">
+        <ShopItemFuncClassBased item ={item} />
+      </div>
     </div>
   );
 }
